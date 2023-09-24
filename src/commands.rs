a0f2e44ebd0b13894a771552fe28f722fabfdb9a
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-09-25 00:45:16 +0100

    Verify mostly working?

--- src/commands.rs
+++ src/commands.rs
@@ -1,14 +1,79 @@
-use libsums::member::StudentId;
+use std::sync::Mutex;
 
-pub struct Data {} // User data, which is stored and accessible in all command invocations
+use libsums::{
+    client::SumsClient,
+    member::{Member, StudentId},
+};
+use poise::serenity_prelude::RoleId;
+
+pub struct Data {
+    pub role_id: RoleId,
+    pub members: Mutex<Vec<Member>>,
+    pub group_id: u16,
+    pub webdriver_address: String,
+    pub sums_username: String,
+    pub sums_password: String,
+}
+
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[poise::command(slash_command)]
+#[poise::command(slash_command, ephemeral)]
 pub async fn verify(
     ctx: Context<'_>,
     #[description = "Your Student ID"] student_id: StudentId,
 ) -> Result<(), Error> {
-    ctx.say(format!("Your student ID is {student_id}")).await?;
+    // We do these awkward scoped accesses to members because Rust doesn't like
+    // holding mutexes past async calls
+    let is_member = {
+        let members = ctx.data().members.lock().unwrap();
+        members
+            .iter()
+            .any(|student| student.student_id == student_id)
+    };
+
+    if is_member {
+        add_role(ctx).await?;
+
+        return Ok(());
+    }
+
+    ctx.defer_ephemeral().await?;
+
+    let client = SumsClient::new(ctx.data().group_id, &ctx.data().webdriver_address).await?;
+
+    client
+        .authenticate(&ctx.data().sums_username, &ctx.data().sums_password)
+        .await?;
+
+    let new_student_list = client.members().await?;
+
+    let is_now_in_members = new_student_list
+        .iter()
+        .any(|student| student.student_id == student_id);
+
+    *ctx.data().members.lock().unwrap() = new_student_list;
+
+    if is_now_in_members {
+        add_role(ctx).await?;
+
+        return Ok(());
+    }
+
+    ctx.say("Not found :(").await?;
+
+    Ok(())
+}
+
+async fn add_role(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("Verified! Welcome to HackSoc :)").await?;
+
+    if let Some(mut author) = ctx.author_member().await {
+        author
+            .to_mut()
+            .add_role(ctx.http(), ctx.data().role_id)
+            .await?;
+    }
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod commands;
 
+use std::{env, sync::Mutex};
+
 use commands::{verify, Data};
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{GatewayIntents, RoleId};
 
 #[tokio::main]
 async fn main() {
@@ -10,12 +12,28 @@ async fn main() {
             commands: vec![verify()],
             ..Default::default()
         })
-        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
-        .intents(serenity::GatewayIntents::non_privileged())
+        .token(env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN"))
+        .intents(GatewayIntents::non_privileged() | GatewayIntents::GUILD_MEMBERS)
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data {
+                    role_id: RoleId(
+                        env::var("ROLE_ID")
+                            .expect("Missing ROLE_ID")
+                            .parse()
+                            .expect("Invalid ROLE_ID"),
+                    ),
+                    members: Mutex::new(vec![]),
+                    group_id: env::var("GROUP_ID")
+                        .expect("Missing GROUP_ID")
+                        .parse()
+                        .expect("Invalid GROUP_ID"),
+                    webdriver_address: env::var("WEBDRIVER_ADDRESS")
+                        .expect("Missing WEBDRIVER_ADDRESS"),
+                    sums_username: env::var("SUMS_USERNAME").expect("Missing SUMS_USERNAME"),
+                    sums_password: env::var("SUMS_PASSWORD").expect("Missing SUMS_PASSWORD"),
+                })
             })
         });
 
